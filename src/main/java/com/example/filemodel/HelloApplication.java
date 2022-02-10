45c1cc925220a4e1218f09e2eed79a8847cdfc05
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-10 14:17:42 -0500

    detects a file if exists

--- src/main/java/com/example/filemodel/HelloApplication.java
+++ src/main/java/com/example/filemodel/HelloApplication.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -25,10 +25,19 @@ public class HelloApplication extends Application {
     String path = "C:\\Users\\james.keogh\\Desktop\\templates\\Java.fx\\";
     String filename = "out.wav";
     String fileOut = path + filename;
-    FileReader fileReader = new FileReader(filename);
 
-    FileWriter fileWriter = new FileWriter(filename);
+    File file = new File(path + filename);
+    FileWriter fileWriter = new FileWriter(file);
+    int numerator = 1;
+    String versionNumber = "(" + numerator + ")";
 
-    fileWriter.write (fileOut);
+    if (file.createNewFile()) {
+      System.out.println("We can write the file !!!");
+    } else {
+      System.out.println("FILE IS BLOODY FOUND FFS!!!");
+    }
+
+    fileWriter.write(fileOut);
+    System.out.println("file out " + file);
   }
 }
