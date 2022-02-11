5145829d5a8a149c62a46b00ef669dd926d361e5
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-11 14:44:25 -0500

    Puts the method into a Class as a static method.(neater)

---
+++ src/main/java/com/example/filemodel/FileCtr.java
@@ -0,0 +1,34 @@
+package com.example.filemodel;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileCtr {
+
+    public FileCtr() {
+    }
+
+  static  void checkSequenceFiles() throws IOException {
+        String path = "C:\\Users\\james.keogh\\Desktop\\templates\\Java.fx\\";
+        String filename = "out";
+        String extension = ".wav";
+
+        int versionNumber = 0;
+        String fileOut = path + filename + extension;
+        File file = new File(fileOut);
+
+        while (file.exists()) {
+            System.out.println("file exists");
+            versionNumber++;
+            fileOut = path + filename + "(" + versionNumber + ")" + extension;
+            file = new File(fileOut);
+        }
+
+        System.out.println("file out " + fileOut);
+        //  FileWriter fileWriter = new FileWriter(fileOut);
+        new FileWriter (fileOut);
+    }
+
+
+}
--- src/main/java/com/example/filemodel/HelloApplication.java
+++ src/main/java/com/example/filemodel/HelloApplication.java
@@ -22,28 +22,7 @@ public class HelloApplication extends Application {
     stage.setScene(scene);
     stage.show();
     //////////////////////////////////////////
-    checkSequenceFiles ();
-
+    FileCtr.checkSequenceFiles();
   }
 
-  private void checkSequenceFiles() throws IOException {
-    String path = "C:\\Users\\james.keogh\\Desktop\\templates\\Java.fx\\";
-    String filename = "out";
-    String extension = ".wav";
-
-    int versionNumber = 0;
-    String fileOut = path + filename + extension;
-    File file = new File(fileOut);
-
-    while (file.exists()) {
-      System.out.println("file exists");
-      versionNumber++;
-      fileOut = path + filename + "(" + versionNumber + ")" + extension;
-      file = new File(fileOut);
-    }
-
-    System.out.println("file out " + fileOut);
-    //  FileWriter fileWriter = new FileWriter(fileOut);
-    new FileWriter (fileOut);
-  }
 }
