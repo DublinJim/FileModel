333ac7c22e0be7687b5397e0831ba931e6e14b3d
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-16 15:22:13 -0500

    adds a gui interface

--- src/main/java/com/example/filemodel/FileCtr.java
+++ src/main/java/com/example/filemodel/FileCtr.java
@@ -6,23 +6,43 @@ import java.io.IOException;
 
 public class FileCtr {
 
-  static void checkSequenceFiles() throws IOException {
-    String path = "C:\\Users\\james.keogh\\Desktop\\templates\\Java.fx\\";
-    String filename = "out";
-    String extension = ".wav";
+    static String fileOut = "blank";
+    static String filename = "out";
+    static String extension = ".wav";
+    static int versionNumber = 0;
 
-    int versionNumber = 0;
-    String fileOut = (path + filename + extension);
-    File file = new File(fileOut);
-
-    while (file.exists()) {
-      System.out.println("file exists");
-      versionNumber++;
-      fileOut = path + filename + "(" + versionNumber + ")" + extension;
-      file = new File(fileOut);
+    public FileCtr() {
     }
 
-    System.out.println("file out " + fileOut);
-    new FileWriter(fileOut);
-  }
+
+    static void checkSequenceFiles() throws IOException {
+        String path = "C:\\Users\\james.keogh\\Desktop\\templates\\Java.fx\\";
+
+        fileOut = (path + filename + extension);
+
+        File file = new File(fileOut);
+
+        while (file.exists()) {
+            System.out.println("file exists");
+
+            versionNumber++;
+            fileOut = path + filename + "(" + versionNumber + ")" + extension;
+            file = new File(fileOut);
+        }
+
+        System.out.println("file out " + fileOut);
+        new FileWriter(fileOut);
+
+
+    }
+
+
+    public static String checkFileExists() {
+        return filename + extension;
+    }
+
+    public static String fileToWrite() {
+        return filename + "(" + versionNumber + ")" + extension;
+    }
+
 }
--- src/main/java/com/example/filemodel/HelloController.java
+++ src/main/java/com/example/filemodel/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.filemodel;
 
-import javafx.application.Platform;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class HelloController {
 
     @FXML
-    void quitApp() {
-        Platform.exit ();
+    private Text fileFoundTxt;
+
+    @FXML
+    private Text newFileTxt;
+
+    @FXML
+    private Button quitOut;
+
+    @FXML
+    void getTheFiles(ActionEvent event) throws IOException {
+        FileCtr.checkSequenceFiles();
+        fileFoundTxt.setText(FileCtr.checkFileExists());
+        newFileTxt.setText(FileCtr.fileToWrite());
     }
 
 }
